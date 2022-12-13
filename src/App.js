ebf506b957c09da27a4c9b5f2c143a6762c885a1
SOAR <nhutthang.t@gmail.com>
2022-12-13 16:22:53 +0700

    [Learn] about map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          id: "123",
+          name: "Soar",
+        },
+        {
+          id: "124",
+          name: "Solid",
+        },
+        {
+          id: "125",
+          name: "Vivian",
+        },
+        {
+          id: "121",
+          name: "Vivian",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
 }
-
 export default App;
