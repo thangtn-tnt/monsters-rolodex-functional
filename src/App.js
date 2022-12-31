5a30e83697cad2ebc51134047cb64e301ec88fea
SOAR <nhutthang.t@gmail.com>
2022-12-31 09:34:53 +0700

    [Implement] Search monsters

--- src/App.js
+++ src/App.js
@@ -6,30 +6,57 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: "123",
-          name: "Soar",
-        },
-        {
-          id: "124",
-          name: "Solid",
-        },
-        {
-          id: "125",
-          name: "Vivian",
-        },
-        {
-          id: "121",
-          name: "Vivian",
-        },
-      ],
+      monsters: [],
+      searchField: '',
     };
+
+    console.log('constructor');
   }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      })
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+
+    this.setState(
+      () => {
+        return { searchField }
+      },
+    )
+  }
+
   render() {
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filterUsers = monsters.filter((monster) => {
+      return monster.name.includes(searchField);
+    })
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input type="search"
+          placeholder="Search users"
+          className="search-box"
+          onChange={onSearchChange} />
+
+        {filterUsers.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
